90680d6e185b38a16340e34adb12a02ca5f5ef57
Rob Esris <robesris@gmail.com>
2013-02-26 18:17:43 -0500

    Removed rr.  Some basic tests passing.

--- lib/terminal.rb
+++ lib/terminal.rb
@@ -1,3 +1,13 @@
 class Terminal
+  def set_pricing(price_list)
+    @price_list = price_list
+  end
 
+  def price_list(product_name = nil)
+    if product_name
+      @price_list[product_name.to_s.upcase]
+    else
+      @price_list
+    end
+  end
 end
--- spec/terminal_spec.rb
+++ spec/terminal_spec.rb
@@ -2,8 +2,17 @@ require 'terminal'
 
 # basic functionality
 describe Terminal, "#set_pricing" do
-  it "returns an empty price list by default" do
+  it "allows us to set and retrieve pricing" do
     terminal = Terminal.new
-    terminal.set_pricing.should be_empty
+    expect {terminal.set_pricing({'A' => { :price => 200, :bulk_qty => 4, :bulk_price => 700 },
+                                  'B' => { :price => 1200 },
+                                  'C' => { :price => 125, :bulk_qty => 6, :bulk_price => 600 },
+                                  'D' => { :price => 15 }}) }.to_not raise_error
+    terminal.price_list['A'][:price].should == 200
+    terminal.price_list['A'][:bulk_qty].should == 4
+    terminal.price_list['B'][:price].should == 1200
+    terminal.price_list(:c)[:price].should == 125
+    terminal.price_list('C')[:bulk_price].should == 600
+    terminal.price_list('d')[:price].should == 15
   end
 end
