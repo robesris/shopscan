06b1b22a40b73babe39d84f5e16e0d7a93e0b1ea
Rob Esris <robesris@gmail.com>
2013-02-26 20:13:44 -0500

    Added a test to check all carts of length n.

--- spec/terminal_spec.rb
+++ spec/terminal_spec.rb
@@ -55,4 +55,74 @@ describe Terminal do
       terminal.total.should == test_case[:expected]
     end
   end
+
+  it "correctly totals all 'carts' of length #{ENV['LIST_LENGTH'] || 1}" do
+    list_length = ENV['LIST_LENGTH'] || 1
+
+    terminal = Terminal.new
+    terminal.set_pricing(default_prices)
+    
+    item_names = terminal.price_list.map { |key, item| key }
+
+    list_length = list_length.to_i
+    if list_length >= 1
+      old_combos = [ { :list => "", :value => 0, 'A' => 0, 'B' => 0, 'C' => 0, 'D' => 0 } ]
+      1.upto(list_length) do |length|
+        new_combos = []
+        puts "\nLENGTH #{length}:"
+        old_combos.each do |old_combo|
+          item_names.each do |item_name|
+            new_combo = { :list => old_combo[:list] + item_name, 
+                          :value => nil, # we're going to recalculate
+                          'A' => old_combo['A'],
+                          'B' => old_combo['B'],
+                          'C' => old_combo['C'],
+                          'D' => old_combo['D']
+                        }
+            new_combo[item_name] += 1
+            
+            combo_value = 0
+
+            terminal = Terminal.new
+            terminal.set_pricing(default_prices)
+            'A'.upto('D') do |combo_item_name|
+              combo_item = terminal.price_list[combo_item_name]
+              bulk_qty = combo_item[:bulk_qty]
+              bulk_price = combo_item[:bulk_price]
+              remaining = new_combo[combo_item_name]
+
+              if bulk_qty && bulk_price
+                combo_value += remaining / bulk_qty * bulk_price
+                remaining %= bulk_qty
+              end
+              combo_value += remaining * combo_item[:price]
+            end
+            new_combo[:value] = combo_value
+
+            # now scan and check against expected values
+            items = new_combo[:list].split ""
+            items.each do |item|
+              terminal.scan(item)
+            end
+
+            # moment of truth...
+            grand_total = terminal.total
+            result_s = if grand_total == new_combo[:value]
+              "*** PASSED:"
+            else
+              "!!! FAILED:"
+            end
+
+            puts "#{result_s} Expected: #{new_combo[:value]}\tActual: #{grand_total}\tCart: #{new_combo[:list]}"
+
+            # the actual test
+            grand_total.should == new_combo[:value]
+
+            new_combos << new_combo
+          end
+        end
+        old_combos = new_combos
+      end
+    end
+  end
 end
